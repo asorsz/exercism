44198a16d4e4d9a023b49522685949c56b319403
asorsz <asorsz@gmail.com>
2021-02-05 14:30:34 +0000

    Complete space-age core exercise

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,15 @@
+export const age = (planet, seconds) =>
+  Math.round(seconds / secsPerEarthYear / earthYearEquivs[planet] * 100) / 100;
+
+export const secsPerEarthYear = 31557600;
+
+export const earthYearEquivs = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1.0, 
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
